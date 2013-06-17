2e463c59da9fc9a06b6b2c11c5532f6cca0c4830
Mark Hobson <markhobson@gmail.com>
2013-06-17 21:46:28 +0100

    Added contacts resource

---
+++ src/main/java/org/hobsoft/contacts/ContactsResource.java
@@ -0,0 +1,17 @@
+package org.hobsoft.contacts;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("contacts")
+public class ContactsResource
+{
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String contacts()
+	{
+		return "<html><body><h1>Contacts</h1></body></html>";
+	}
+}
---
+++ src/test/java/org/hobsoft/contacts/ContactsResourceTest.java
@@ -0,0 +1,31 @@
+package org.hobsoft.contacts;
+
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ContactsResourceTest extends JerseyTest
+{
+	// JerseyTest methods -----------------------------------------------------
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected Application configure()
+	{
+		return new ResourceConfig(ContactsResource.class);
+	}
+	
+	@Test
+	public void contacts()
+	{
+		String actual = target("contacts").request().get(String.class);
+		
+		assertEquals("<html><body><h1>Contacts</h1></body></html>", actual);
+	}
+}
