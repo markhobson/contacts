1a1222ce7d50dc8531ca4f8c978058bb0231509d
Mark Hobson <markhobson@gmail.com>
2013-06-20 21:51:41 +0100

    Moved contacts HTML into separate file

--- server/src/main/java/org/hobsoft/contacts/server/ContactsResource.java
+++ server/src/main/java/org/hobsoft/contacts/server/ContactsResource.java
@@ -1,17 +1,21 @@
 package org.hobsoft.contacts.server;
 
+import java.io.InputStream;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("contacts")
 public class ContactsResource
 {
 	@GET
 	@Produces(MediaType.TEXT_HTML)
-	public String contacts()
+	public Response contacts()
 	{
-		return "<html><body><h1>Contacts</h1></body></html>";
+		InputStream in = getClass().getResourceAsStream("contacts.html");
+		return Response.ok(in, MediaType.TEXT_HTML).build();
 	}
 }
