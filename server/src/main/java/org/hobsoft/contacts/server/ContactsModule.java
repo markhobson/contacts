1684e5a6a972cb4bd17a5756212d7ea96c1c27ae
Mark Hobson <markhobson@gmail.com>
2013-06-27 21:29:37 +0100

    Removed the need for guice-servlet

--- server/src/main/java/org/hobsoft/contacts/server/ContactsModule.java
+++ server/src/main/java/org/hobsoft/contacts/server/ContactsModule.java
@@ -3,21 +3,21 @@ package org.hobsoft.contacts.server;
 import org.hobsoft.contacts.server.dao.ContactDao;
 import org.hobsoft.contacts.server.dao.FakeContactDao;
 
+import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
-import com.google.inject.servlet.ServletModule;
 
 /**
  * Main Guice module.
  */
-public class ContactsModule extends ServletModule
+public class ContactsModule extends AbstractModule
 {
-	// ServletModule methods --------------------------------------------------
+	// AbstractModule methods -------------------------------------------------
 	
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-	protected void configureServlets()
+	protected void configure()
 	{
 		bind(ContactDao.class).to(FakeContactDao.class).in(Scopes.SINGLETON);
 	}
