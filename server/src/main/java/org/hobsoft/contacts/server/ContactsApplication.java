519121177536120a3e6051dd3eb216d2915b74fa
Mark Hobson <markhobson@gmail.com>
2013-06-27 20:56:17 +0100

    Removed root resource

--- server/src/main/java/org/hobsoft/contacts/server/ContactsApplication.java
+++ server/src/main/java/org/hobsoft/contacts/server/ContactsApplication.java
@@ -3,13 +3,13 @@ package org.hobsoft.contacts.server;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.mvc.freemarker.FreemarkerMvcFeature;
 import org.glassfish.jersey.server.mvc.freemarker.FreemarkerProperties;
-import org.hobsoft.contacts.server.resource.RootResource;
+import org.hobsoft.contacts.server.resource.ContactsResource;
 
 public class ContactsApplication extends ResourceConfig
 {
 	public ContactsApplication()
 	{
-		packages(RootResource.class.getPackage().getName());
+		packages(ContactsResource.class.getPackage().getName());
 		
 		register(FreemarkerMvcFeature.class);
 		
--- server/src/main/java/org/hobsoft/contacts/server/resource/ContactsResource.java
+++ server/src/main/java/org/hobsoft/contacts/server/resource/ContactsResource.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -11,6 +12,7 @@ import org.glassfish.jersey.server.mvc.Viewable;
 import org.hobsoft.contacts.server.dao.ContactDao;
 import org.hobsoft.contacts.server.dao.FakeContactDao;
 
+@Path("/contacts")
 public class ContactsResource
 {
 	// fields -----------------------------------------------------------------
