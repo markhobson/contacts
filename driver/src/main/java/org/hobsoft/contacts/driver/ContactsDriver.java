30c4c2af2b0cdd9b3cdca10e49b04d46571f8e8c
Mark Hobson <markhobson@gmail.com>
2013-06-29 11:10:33 +0100

    Allowed integration test port to be configured and changed to 9998

--- driver/src/main/java/org/hobsoft/contacts/driver/ContactsDriver.java
+++ driver/src/main/java/org/hobsoft/contacts/driver/ContactsDriver.java
@@ -1,5 +1,8 @@
 package org.hobsoft.contacts.driver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,15 +11,27 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 public class ContactsDriver
 {
+	// constants --------------------------------------------------------------
+	
+	private static final String DEFAULT_URL = "http://localhost:8080/";
+	
 	// fields -----------------------------------------------------------------
 	
 	private final WebDriver driver;
+	
+	private final URL serverUrl;
 	
 	// constructors -----------------------------------------------------------
 	
 	public ContactsDriver(WebDriver driver)
 	{
+		this(driver, url(DEFAULT_URL));
+	}
+	
+	public ContactsDriver(WebDriver driver, URL serverUrl)
+	{
 		this.driver = checkNotNull(driver, "driver");
+		this.serverUrl = checkNotNull(serverUrl, "serverUrl");
 	}
 	
 	// public methods ---------------------------------------------------------
@@ -28,7 +43,7 @@ public class ContactsDriver
 	
 	public void contacts()
 	{
-		driver.get("http://localhost:8080/contacts");
+		driver.get(url(serverUrl, "/contacts").toString());
 	}
 	
 	public String getHeader()
@@ -47,5 +62,31 @@ public class ContactsDriver
 		}
 		
 		return false;
+	}
+	
+	// private methods --------------------------------------------------------
+
+	private static URL url(String spec)
+	{
+		try
+		{
+			return new URL(spec);
+		}
+		catch (MalformedURLException exception)
+		{
+			throw new IllegalArgumentException(exception);
+		}
+	}
+
+	private static URL url(URL context, String spec)
+	{
+		try
+		{
+			return new URL(context, spec);
+		}
+		catch (MalformedURLException exception)
+		{
+			throw new IllegalArgumentException(exception);
+		}
 	}
 }
--- tests/src/test/java/org/hobsoft/contacts/test/ContactsIT.java
+++ tests/src/test/java/org/hobsoft/contacts/test/ContactsIT.java
@@ -1,5 +1,8 @@
 package org.hobsoft.contacts.test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.hobsoft.contacts.driver.ContactsDriver;
 import org.junit.After;
 import org.junit.Before;
@@ -11,12 +14,22 @@ import static org.junit.Assert.assertTrue;
 
 public class ContactsIT
 {
+	private static final String DEFAULT_SERVER_PROTOCOL = "http";
+	
+	private static final String DEFAULT_SERVER_HOST = "localhost";
+	
+	private static final String SERVER_PORT_PROPERTY = "serverPort";
+	
+	private static final String DEFAULT_SERVER_PORT = "8080";
+	
+	private static final String DEFAULT_SERVER_PATH = "/";
+	
 	private ContactsDriver driver;
 	
 	@Before
-	public void setUp()
+	public void setUp() throws MalformedURLException
 	{
-		driver = new ContactsDriver(new FirefoxDriver());
+		driver = new ContactsDriver(new FirefoxDriver(), getServerUrl());
 	}
 	
 	@After
@@ -41,5 +54,15 @@ public class ContactsIT
 		assertTrue(driver.hasContact("A"));
 		assertTrue(driver.hasContact("B"));
 		assertTrue(driver.hasContact("C"));
+	}
+	
+	private static URL getServerUrl() throws MalformedURLException
+	{
+		String protocol = DEFAULT_SERVER_PROTOCOL;
+		String host = DEFAULT_SERVER_HOST;
+		int port = Integer.valueOf(System.getProperty(SERVER_PORT_PROPERTY, DEFAULT_SERVER_PORT));
+		String path = DEFAULT_SERVER_PATH;
+		
+		return new URL(protocol, host, port, path);
 	}
 }
