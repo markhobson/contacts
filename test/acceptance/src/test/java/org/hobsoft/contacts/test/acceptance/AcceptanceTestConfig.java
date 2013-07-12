e40880986bb2f940ffd621e1c0bf42e64deb41df
Mark Hobson <markhobson@gmail.com>
2013-07-12 23:46:46 +0100

    Aligned package name with new module name

--- test/acceptance/src/test/java/org/hobsoft/contacts/test/acceptance/AcceptanceTestConfig.java
+++ test/acceptance/src/test/java/org/hobsoft/contacts/test/acceptance/AcceptanceTestConfig.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.hobsoft.contacts.test;
+package org.hobsoft.contacts.test.acceptance;
 
 import java.net.MalformedURLException;
 import java.net.URL;
--- test/acceptance/src/test/java/org/hobsoft/contacts/test/acceptance/ContactsIT.java
+++ test/acceptance/src/test/java/org/hobsoft/contacts/test/acceptance/ContactsIT.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.hobsoft.contacts.test;
+package org.hobsoft.contacts.test.acceptance;
 
 import org.hobsoft.contacts.driver.ContactsDriver;
 import org.hobsoft.contacts.driver.SignInDriver;
