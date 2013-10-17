e646c361c043f0376fa36f7dcd7323105a47bc42
Mark Hobson <markhobson@gmail.com>
2013-10-17 20:45:59 +0100

    Simplified /favicon.ico security

--- server/src/main/java/org/hobsoft/contacts/server/security/SecurityConfig.java
+++ server/src/main/java/org/hobsoft/contacts/server/security/SecurityConfig.java
@@ -16,7 +16,6 @@ package org.hobsoft.contacts.server.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -35,19 +34,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 	 * {@inheritDoc}
 	 */
 	@Override
-	public void configure(WebSecurity web) throws Exception
-	{
-		web.ignoring()
-			.antMatchers("/favicon.ico");
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
 	protected void configure(HttpSecurity http) throws Exception
 	{
 		http.authorizeUrls()
+				.antMatchers("/favicon.ico").permitAll()
 				.antMatchers("/webjars/**").permitAll()
 				.anyRequest().authenticated()
 				.and()
