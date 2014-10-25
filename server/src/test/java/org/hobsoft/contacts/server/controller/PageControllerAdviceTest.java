5c1f608838503fbd09c8752d2005c42fcf6cb885
Mark Hobson <markhobson@gmail.com>
2014-10-25 22:26:18 +0100

    Replaced Spring Security JSP tags with JSTL

---
+++ server/src/test/java/org/hobsoft/contacts/server/controller/PageControllerAdviceTest.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hobsoft.contacts.server.controller;
+
+import java.security.Principal;
+import java.util.Collections;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Tests {@code PageControllerAdvice}.
+ */
+public class PageControllerAdviceTest
+{
+	// ----------------------------------------------------------------------------------------------------------------
+	// fields
+	// ----------------------------------------------------------------------------------------------------------------
+
+	private PageControllerAdvice advice;
+	
+	// ----------------------------------------------------------------------------------------------------------------
+	// public methods
+	// ----------------------------------------------------------------------------------------------------------------
+
+	@Before
+	public void setUp()
+	{
+		advice = new PageControllerAdvice();
+	}
+
+	// ----------------------------------------------------------------------------------------------------------------
+	// tests
+	// ----------------------------------------------------------------------------------------------------------------
+
+	@Test
+	public void getUserWithAuthenticationReturnsUser()
+	{
+		User user = newUser();
+		Principal principal = new TestingAuthenticationToken(user, null);
+
+		User actual = advice.getUser(principal);
+		
+		assertThat(actual, is(user));
+	}
+	
+	// ----------------------------------------------------------------------------------------------------------------
+	// private methods
+	// ----------------------------------------------------------------------------------------------------------------
+
+	private static User newUser()
+	{
+		return new User("_username", "_password", Collections.<GrantedAuthority>emptySet());
+	}
+}
