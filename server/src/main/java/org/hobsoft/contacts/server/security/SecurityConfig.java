8a1af239be3024ed86fe6278bc8c26e4c156937f
Mark Hobson <markhobson@gmail.com>
2015-05-26 20:59:11 +0100

    Upgraded Spring Security to 4.0.1

--- server/src/main/java/org/hobsoft/contacts/server/security/SecurityConfig.java
+++ server/src/main/java/org/hobsoft/contacts/server/security/SecurityConfig.java
@@ -16,14 +16,14 @@ package org.hobsoft.contacts.server.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 /**
  * Spring Security configuration.
  */
 @Configuration
-@EnableWebMvcSecurity
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
 	// ----------------------------------------------------------------------------------------------------------------
