da542845f260967fe8c5bc54bba8da010e1f690b
Mark Hobson <markhobson@gmail.com>
2016-03-03 08:15:35 +0000

    Moved root controllers to top-level package

--- server/src/main/java/org/hobsoft/contacts/server/RootController.java
+++ server/src/main/java/org/hobsoft/contacts/server/RootController.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.hobsoft.contacts.server.controller;
+package org.hobsoft.contacts.server;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
